7eaf8c46fdd51a2cd5177cf1b986dd94cd09eac4
bis5 <bis5@bis5.mydns.jp>
2013-10-24 11:43:40 +0900

    first commit

---
+++ my_wishlist.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:my_wishlist) do
+  command(:insert_wishlist,
+    name: '欲しい物リストのURLを挿入',
+    condition: lambda{ |opt| true },
+    visible: true,
+    role: :postbox
+  ) do |opt|
+    begin
+      message = Plugin[:gtk].widgetof(opt.widget).widget_post.buffer.text
+      url = "http://www.amazon.co.jp/registry/wishlist/#{UserConfig[:my_wishlist_url]}"
+      if UserConfig[:my_wishlist_insert_before] then
+        message = "#{url} #{message}"
+      else
+        message = "#{message} #{url}"
+      end
+      Plugin[:gtk].widgetof(opt.widget).widget_post.buffer.text = message
+    end
+  end
+  settings "wishlist" do
+    input('欲しい物リストのID', :my_wishlist_url)
+      tooltip('欲しい物リストの画面で「このリストをシェアする」をクリックし、固定URLの~/wishlist/以降をコピペしてください')
+    boolean('postboxに入力されたテキストの前に挿入する場合はチェック', :my_wishlist_insert_before)
+  end
+end
---
+++ spec
@@ -0,0 +1,11 @@
+---
+slug: :my_wishlist
+depends:
+  mikutter: 0.2.2.1373
+  plugin: 
+  - settings
+  - gtk
+version: '1.0'
+author: maruTA_bis5
+name: my_wishlist
+description: 自分Amazon 欲しい物リストのURLをpostboxに挿入する
